9d6e92c8a9b8dca205f10e5f62141ca4f4328a36
Lauren Yoo <laurenyoo620@gmail.com>
2026-04-11 17:21:18 -0700

    Added CORS headers

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express')
 require('dotenv').config()
 
 const app = express()
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+  if (req.method === 'OPTIONS') return res.sendStatus(204)
+  next()
+})
+
 app.use(express.json())
 
 const linksRouter = require('./routes/links')
